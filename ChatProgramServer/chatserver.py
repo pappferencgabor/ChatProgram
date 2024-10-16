3b0ffb8967c21c9a1a056f68dee8df5d8c73bee8
pappferencgabor <p.feco2005@gmail.com>
2024-10-16 09:24:52 +0200

    server

---
+++ ChatProgramServer/chatserver.py
@@ -0,0 +1,53 @@
+import uvicorn
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+
+app = FastAPI()
+
+origins = [
+    "*",
+    "http://localhost",
+    "http://localhost:7777"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/chat")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=7777)
---
+++ ChatProgramServer/install.bat
@@ -0,0 +1,4 @@
+@echo off
+:: Install package
+pip install -r requirements.txt
+pause
